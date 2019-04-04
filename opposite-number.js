bbaff197f4acf41f96b3bca294961b45dc1daab6
Yurii Shkoropad <lvivgeorge@gmail.com>
2019-04-04 22:30:20 +0300

    Create opposite-number.js

---
+++ opposite-number.js
@@ -0,0 +1,14 @@
+// Opposite number
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function oppositeNumber(n) {}
+
+console.log(oppositeNumber(1) === -1);
+console.log(oppositeNumber(14) === -41);
+console.log(oppositeNumber(34) === -34);
