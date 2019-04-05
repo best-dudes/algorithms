e7032be99f3353b6b2b47474db60c563e974c85e
Yurii Shkoropad <lvivgeorge@gmail.com>
2019-04-05 23:25:32 +0300

    Create shout.js

---
+++ shout.js
@@ -0,0 +1,8 @@
+// Shout
+
+function shout(str) {
+}
+
+console.log(shout('help me') === 'Help me!!!');
+console.log(shout('i like you') === 'I like you!!!');
+console.log(shout('you are crazy') === 'You are crazy!!!');
