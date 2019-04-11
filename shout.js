4b3e3e817388d774d4eac5e3f75168dd364eee93
Dmytro Mykhalenych <41123786+dm-mykhalenych@users.noreply.github.com>
2019-04-11 11:08:14 +0300

    Update shout.js

--- shout.js
+++ shout.js
@@ -1,15 +1,9 @@
 // Shout
 
 function shout(str) {
-<<<<<<< HEAD
-    if (!str) return str;
     return str[0].toUpperCase() + str.slice(1) + '!!!';
 }
       
-=======
-}
->>>>>>> e7032be99f3353b6b2b47474db60c563e974c85e
-
 console.log(shout('help me') === 'Help me!!!');
 console.log(shout('i like you') === 'I like you!!!');
 console.log(shout('you are crazy') === 'You are crazy!!!');
