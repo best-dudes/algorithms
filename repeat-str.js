99e7f2c80bca441ccb978e8acaafaff5a984dd35
Yurii Shkoropad <lvivgeorge@gmail.com>
2019-04-11 13:08:20 +0300

    Create repeat-str.js

---
+++ repeat-str.js
@@ -0,0 +1,8 @@
+// repeatStr
+
+function repeatStr(times, str) {
+}
+      
+console.log(repeatStr(2, 'Hello') === 'Hello Hello');
+console.log(repeatStr(3, 'i like you') === 'i like you i like you i like you');
+console.log(repeatStr(9, 'ahah') === 'ahah ahah ahah ahah ahah ahah ahah ahah ahah');
