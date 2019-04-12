9d2b2bd67f51eed8fc276269c02896ba1fb71ad7
Dmytro Mykhalenych <41123786+dm-mykhalenych@users.noreply.github.com>
2019-04-12 23:41:31 +0300

    repeat

--- shout.js
+++ shout.js
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
+function shout(str) {
+    return str[0].toUpperCase() + str.slice(1) + '!!!';
+}
+      
+console.log(shout('help me') === 'Help me!!!');
+console.log(shout('i like you') === 'I like you!!!');
+console.log(shout('you are crazy') === 'You are crazy!!!');
+=======
 // Shout
 
 function shout(str) {
@@ -7,3 +16,4 @@ function shout(str) {
 console.log(shout('help me') === 'Help me!!!');
 console.log(shout('i like you') === 'I like you!!!');
 console.log(shout('you are crazy') === 'You are crazy!!!');
+>>>>>>> 99e7f2c80bca441ccb978e8acaafaff5a984dd35
