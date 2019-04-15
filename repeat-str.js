0e277bb2f143ba0076ece0c0ee60679607677553
Yurii Shkoropad <lvivgeorge@gmail.com>
2019-04-15 22:51:14 +0300

    Update repeat-str.js

--- repeat-str.js
+++ repeat-str.js
@@ -10,6 +10,19 @@ function repeatStr(times, str) {
 	} 	
 	return repeatedString.trimEnd();
 }
+
+function repeatStr(times, str) {
+	var repeatedString = '';
+	for (var i = 0; i < times; i++) {
+		repeatedString += str;
+		if (i + 1 < times) {
+			repeatedString += ' ';
+		}
+	} 	
+	return repeatedString;
+}
+
+
       
 console.log(repeatStr(2, 'Hello') === 'Hello Hello');
 console.log(repeatStr(3, 'i like you') === 'i like you i like you i like you');
