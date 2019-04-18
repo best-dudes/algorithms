57d040b82c5a7ea1e476f0a3103ddd0d172c61ad
Dmytro Mykhalenych <41123786+dm-mykhalenych@users.noreply.github.com>
2019-04-18 20:56:11 +0300

    Update likes.js

--- likes.js
+++ likes.js
@@ -10,8 +10,19 @@
 // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 
 function likes(names) {
+	if (names.length === 0) {
+    return 'no one likes this';
+} else if (names.length === 1) {
+    return names [0] + ' likes this';
+} else if (names.length === 2) {
+    return names [0] + ' and ' + names [1] + ' like this';
+} else if (names.length === 3) {
+    return names [0] + ', ' + names [1] + ' and ' + names [2] + ' like this';
+} else if (names.length > 3 ) {
+    return names [0] + ', ' + names [1] + ' and ' + '2 others like this';  
 }
-      
+}
+
 console.log(likes([]) === 'no one likes this');
 console.log(likes(['Peter']) === 'Peter likes this');
 console.log(likes(['Jacob', 'Alex']) === 'Jacob and Alex like this');
