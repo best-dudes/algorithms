532d0ca8ce6ea3269e664956a4428d11e246f735
Dmytro Mykhalenych <41123786+dm-mykhalenych@users.noreply.github.com>
2019-04-23 10:35:21 +0300

    Update is-integer.js

--- is-integer.js
+++ is-integer.js
@@ -7,6 +7,7 @@
 // 1.1: false
 
 function isInteger(n) {
+return Number.isInteger(n);
 }
 
 console.log(isInteger(0) === true);
@@ -14,3 +15,4 @@ console.log(isInteger(1) === true);
 console.log(isInteger(-1) === true);
 console.log(isInteger(1.9) === false);
 console.log(isInteger(0.01) === false);
+
