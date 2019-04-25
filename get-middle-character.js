b7fb45ac0c4fc86f4edd02ea51b655712c73d485
Yurii Shkoropad <lvivgeorge@gmail.com>
2019-04-25 21:40:19 +0300

    Create get-middle-character.js

---
+++ get-middle-character.js
@@ -0,0 +1,19 @@
+// Get Middle Character
+// If word has odd count of characters, return the middle one character
+// If even, return two characters, which mostly in the middle
+
+// Examples:
+
+// test: es
+// testing: t
+// middle: dd
+// A: A
+
+function getMiddleCharacter(n) {
+}
+
+
+console.log(getMiddleCharacter('test') === 'es');
+console.log(getMiddleCharacter('testing') === 't');
+console.log(getMiddleCharacter('middle') === 'dd');
+console.log(getMiddleCharacter('A') === 'A');
